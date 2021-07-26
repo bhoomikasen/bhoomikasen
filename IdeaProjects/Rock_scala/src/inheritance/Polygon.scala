df25bf793af8066843d641f173c7843abe99ed2a
Bhoomika_Sen <bhoomikasengit@gmail.com>
2021-07-26 15:04:01 +0530

    added one method to truanle class

--- IdeaProjects/Rock_scala/src/inheritance/Polygon.scala
+++ IdeaProjects/Rock_scala/src/inheritance/Polygon.scala
@@ -14,9 +14,11 @@ object Polygon{
 
     var  tri =new Triangle(55.2,30.0,0);
     var tri1=new Triangle(2,3,5);
+    var tri2=new Triangle(2,3)
 
     printArea(tri)
     println(tri1.perimeter);
+    println(tri2.area2)
     //println(poly);
 
   }
