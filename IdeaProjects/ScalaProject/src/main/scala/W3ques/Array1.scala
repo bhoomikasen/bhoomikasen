5e7f08578cebe949c48ccb391f5bee9aaa0051ff
Bhoomika_Sen <bhoomikasengit@gmail.com>
2022-04-09 17:30:57 +0530

    added Array Program

--- IdeaProjects/ScalaProject/src/main/scala/W3ques/Array1.scala
+++ IdeaProjects/ScalaProject/src/main/scala/W3ques/Array1.scala
@@ -1,18 +1,37 @@
 package main.scala.W3ques
 
+import scala.util.control.Exception.noCatch.desc
+
 class Array1{
 /*11. Write a Scala program to create
 a new array taking the middle element from three arrays of length 5*/
 
-  def NewArray1(): Unit ={
-    val s1=Array(1,2,3,4,5)
-    val s2=Array(2,2,4,4,5)
-    val s3=Array(4,2,5,4,6)
-    val s4= Array[Int]
-    for (i<-0 to 2){
-      s4=s4.
+    def test(x: Array[Int], y: Array[Int], z: Array[Int]): Array[Int] = {
+      if (x.length != 5 || y.length != 5 || z.length != 5)
+        throw new IllegalArgumentException("Array length not matched!")
+      else Array(x(2), y(2), z(2))//updating array and adding 2nd value from all the arrays
     }
-
+/*
+  Write a Scala program to find the common elements between two arrays of strings.
+*/
+  def common(a1:Array[String],a2:Array[String]): Unit ={
+    for(i <- 0 to a1.length-1){
+      for (j<-0 to a2.length-1){
+        if(a1(i)==a2(j)){
+          println("\n"+"common elements from both the Arrays are : "+a1(i),a2(j))
+        }
+      }
+    }
+  }
   }
 
+object objArray1 extends App{
+  val o1=new Array1
+    var result1 = o1.test(Array(1,2,3,4,5),Array(2,3,4,5,6),Array(3,4,5,6,7))
+    // Print all the array elements
+    println("New array:")
+    for ( x <- result1 ) {
+      print(s"${x}, ")
+    }
+  o1.common(Array("hey","hi","hello"),Array("hey","hh","hello"))
 }
